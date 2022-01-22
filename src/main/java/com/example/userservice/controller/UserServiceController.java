0bc9a8636e43c1e928bb659a550dc35cbee48c5d
dev-hjkim <dev--test@naver.com>
2022-01-23 00:05:07 +0900

    220123, make controller

---
+++ src/main/java/com/example/userservice/controller/UserServiceController.java
@@ -0,0 +1,14 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserServiceController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the User Service.";
+    }
+}
