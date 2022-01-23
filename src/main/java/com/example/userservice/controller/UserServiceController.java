193d1f7f72123d1693a49ceb168165d857e411c6
dev-hjkim <dev--test@naver.com>
2022-01-24 01:58:38 +0900

    220124, add message method for gateway test

--- src/main/java/com/example/userservice/controller/UserServiceController.java
+++ src/main/java/com/example/userservice/controller/UserServiceController.java
@@ -1,14 +1,23 @@
 package com.example.userservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the User Service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("user-request") String header) {
+        log.info(header);
+        return "Hello World in User Service.";
     }
 }
